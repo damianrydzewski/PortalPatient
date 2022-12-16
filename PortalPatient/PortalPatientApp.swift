c66b6ea937ef2b1586d795cbde6d04ac26a2e767
Damian <damianrydzewski.dev@gmail.com>
2022-12-16 02:47:23 +0100

    Initial Commit

---
+++ PortalPatient/PortalPatientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalPatientApp.swift
+//  PortalPatient
+//
+//  Created by Damian on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortalPatientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
